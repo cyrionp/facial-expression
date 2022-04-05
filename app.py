1fc5dfc6121937f43c08876c53eeaa0c8e1bfaf9
Abdulkadir Durmaz <43341682+cyrionp@users.noreply.github.com>
2022-04-05 18:22:02 +0300

    port issue 3

--- app.py
+++ app.py
@@ -103,6 +103,5 @@ def Prediction(filename):
 
 
 if __name__ == "__main__":
-    #port = int(os.getenv('PORT'))
-    #app.run(port=port)
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)

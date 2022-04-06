922d6a58eaaafff0e75fd7dabc51bcbb450f5e17
Abdulkadir Durmaz <43341682+cyrionp@users.noreply.github.com>
2022-04-06 10:41:25 +0300

    updating procfile with run:app

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ def Prediction(filename):
     return f"Yüz ifadeniz tahminen: {my_expressions[ind]}"
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     #port = int(os.environ.get('PORT', 5000))
     #app.run(host='0.0.0.0', port=port)
     app.run()

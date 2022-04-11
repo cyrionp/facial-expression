854265f09862dfd7143fa0fb1118f929a53628de
Abdulkadir Durmaz <43341682+cyrionp@users.noreply.github.com>
2022-04-11 11:47:51 +0300

    update the nomenclature to pep8

--- my_app.py
+++ my_app.py
@@ -10,9 +10,10 @@ from mtcnn import MTCNN
 from PIL import Image
 import time
 
-uploads_dir = "./static/uploads/"
-faces_dir = "./static/faces/"
-allowed_files = {"png", "jpg", "jpeg"}
+UPLOADS_DIR = "./static/uploads/"
+FACES_DIR = "./static/faces/"
+ALLOWED_FILES = {"png", "jpg", "jpeg"}
+EXPRESSION_CATEGORIES = ("kızgın", "iğrenmiş", "korkmuş", "mutlu", "üzgün", "şaşırmış", "nötr")
 
 config = {
     "DEBUG": False,
@@ -27,8 +28,8 @@ app.config.from_mapping(config)
 cache = Cache(app)
 
 
-def isAllowed(filename):
-    return "." in filename and filename.rsplit(".", 1)[1].lower() in allowed_files
+def is_allowed(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_FILES
 
 
 @app.route("/")
@@ -45,12 +46,12 @@ def upload():
     if file.filename == "":
         flash("Yüklemek için bir resim seçmediniz")
         return redirect(request.url)
-    if file and isAllowed(file.filename):
+    if file and is_allowed(file.filename):
         filename = secure_filename(file.filename)
-        DeleteFilesInDir(uploads_dir)
-        file.save(os.path.join(uploads_dir, filename))
+        delete_files_in_dir(UPLOADS_DIR)
+        file.save(os.path.join(UPLOADS_DIR, filename))
         flash("Resim yüklendi")
-        flash(Prediction(filename))
+        flash(prediction(filename))
         return render_template("index.html", filename=filename)
     else:
         flash("İzin verilen resim uzantıları: png, jpg, jpeg")
@@ -62,13 +63,13 @@ def display(filename):
     return redirect(url_for("static", filename="uploads/" + filename), code=301)
 
 
-def DeleteFilesInDir(directory):
+def delete_files_in_dir(directory):
     for i in os.listdir(directory):
         os.remove(directory + i)
         print(i + " silindi")
 
 
-def VerifyJpeg(path):
+def verify_jpeg(path):
     try:
         img = Image.open(path)
         img.getdata()[0]
@@ -77,21 +78,21 @@ def VerifyJpeg(path):
     return True
 
 
-@cache.cached(key_prefix="LoadModelAndDetector")
-def LoadModelAndDetector():
+@cache.cached(key_prefix="load_model_and_detector")
+def load_model_and_detector():
     global model, detector
+    start_timer = time.time()
     model = keras.models.load_model("mymodel.h5")
+    print(f'load_model_and_detector run süresi: {time.time() - start_timer}')
     detector = MTCNN()
 
 
-def PredictionProcesses(filename):
-    my_expressions = ("kızgın", "iğrenmiş", "korkmuş", "mutlu", "üzgün", "şaşırmış", "nötr")
-
+def prediction_processes(filename):
     start_timer = time.time()
-    LoadModelAndDetector()
-    print(f'LoadModelAndDetector run süresi: {time.time() - start_timer}')
+    load_model_and_detector()
+    print(f'load_model_and_detector run süresi: {time.time() - start_timer}')
 
-    img = cv2.imread(uploads_dir + filename)
+    img = cv2.imread(UPLOADS_DIR + filename)
     detections = detector.detect_faces(img)
 
     if detections:
@@ -101,11 +102,11 @@ def PredictionProcesses(filename):
                 x, y, w, h = face["box"]
                 detected_face = img[int(y):int(y + h), int(x):int(x + w)]
 
-                DeleteFilesInDir(faces_dir)
-                cv2.imwrite(faces_dir + filename, detected_face)
+                delete_files_in_dir(FACES_DIR)
+                cv2.imwrite(FACES_DIR + filename, detected_face)
                 print("Resim kaydedildi: " + filename)
 
-                img = image.load_img(faces_dir + filename, color_mode="grayscale", target_size=(48, 48))
+                img = image.load_img(FACES_DIR + filename, color_mode="grayscale", target_size=(48, 48))
 
                 x = image.img_to_array(img)
                 x = np.expand_dims(x, axis=0)
@@ -119,20 +120,20 @@ def PredictionProcesses(filename):
                         minimum_value = predictions[i]
                         most_index = i
 
-                print('Yüz ifadeniz tahminennn: ', my_expressions[most_index])
-                return f"Yüz ifadeniz tahminen: {my_expressions[most_index]}"
+                print('Yüz ifadeniz tahminennn: ', EXPRESSION_CATEGORIES[most_index])
+                return f"Yüz ifadeniz tahminen: {EXPRESSION_CATEGORIES[most_index]}"
     else:
         return "Resimde yüz bulunamadı"
 
 
-def Prediction(filename):
-    file_extension = os.path.splitext(uploads_dir + filename)[1].lower()
+def prediction(filename):
+    file_extension = os.path.splitext(UPLOADS_DIR + filename)[1].lower()
 
     if file_extension == ".jpeg":
-        if VerifyJpeg(uploads_dir + filename):
-            return PredictionProcesses(filename)
+        if verify_jpeg(UPLOADS_DIR + filename):
+            return prediction_processes(filename)
     else:
-        return PredictionProcesses(filename)
+        return prediction_processes(filename)
 
 
 if __name__ == '__main__':
